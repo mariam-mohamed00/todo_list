7d32c2e532192d9e4a04b605394814ebd60c9427
mariam <mariemmohamed2030@gmail.com>
2024-11-14 23:59:59 +0200

    Home Screen

--- lib/my_theme.dart
+++ lib/my_theme.dart
@@ -8,19 +8,35 @@ class MyTheme{
   static Color blackColor = Color(0xff383838);
   static Color whiteColor = Color(0xffffffff);
   static Color greyColor = Color(0xffC8C9CB);
-  static Color backgoundDark = Color(0xff060E1E);
+  static Color backgroundDark = Color(0xff060E1E);
   static Color blackDark = Color(0xff141922);
 
   static ThemeData lightMode = ThemeData(
     scaffoldBackgroundColor: backgroundLight,
     appBarTheme: AppBarTheme(
       backgroundColor: primaryLight,
-      toolbarHeight: 200,
+      toolbarHeight: 100,
       elevation: 0
     ),
 
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: MyTheme.primaryLight,
+      elevation: 0,
+      shape: StadiumBorder(side: BorderSide(color: MyTheme.whiteColor, width: 5)),
+
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      backgroundColor: Colors.transparent,
+      selectedIconTheme: IconThemeData(color: primaryLight, size: 26),
+      unselectedIconTheme: IconThemeData(color: greyColor, size: 26),
+      elevation: 0,
+      selectedLabelStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: blackColor),
+      unselectedLabelStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: blackColor),
+    ),
     textTheme: TextTheme(
-      titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: whiteColor)
+      titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: whiteColor),
     )
   );
 }
